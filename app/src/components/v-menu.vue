b0a7980ef1e20b124d1a040c7e4969c2fa2fc599
bryantgillespie <hey@bryantgillespie.com>
2026-03-12 18:53:04 -0400

    wip

--- app/src/components/v-menu.vue
+++ app/src/components/v-menu.vue
@@ -149,14 +149,14 @@ function useActiveState() {
 
 	const menuEl = useTemplateRef<HTMLDivElement>('menuEl');
 
-	const { activate: activateFocusTrap, deactivate: deactivateFocusTrap } = useFocusTrap([menuEl, activator], {
-		escapeDeactivates: false,
-		initialFocus: false,
-		fallbackFocus: () => menuEl.value!,
-		returnFocusOnDeactivate: !props.noFocusReturn,
-		allowOutsideClick: true,
-		clickOutsideDeactivates: props.closeOnClick,
-	});
+		const { activate: activateFocusTrap, deactivate: deactivateFocusTrap } = useFocusTrap([menuEl, activator], {
+			escapeDeactivates: false,
+			initialFocus: false,
+			fallbackFocus: () => menuEl.value,
+			returnFocusOnDeactivate: !props.noFocusReturn,
+			allowOutsideClick: true,
+			clickOutsideDeactivates: props.closeOnClick,
+		});
 
 	const isActive = computed<boolean>({
 		get() {
@@ -496,9 +496,9 @@ function usePopper(
 					</div>
 					<div
 						ref="menuEl"
-						tabindex="-1"
 						class="v-menu-content"
 						:class="{ seamless }"
+						tabindex="-1"
 						v-on="{
 							...(closeOnContentClick ? { click: onContentClick } : {}),
 							...(trigger === 'hover' ? { pointerenter: onPointerEnter, pointerleave: onPointerLeave } : {}),
